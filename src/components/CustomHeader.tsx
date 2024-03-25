cc8e1753046bebe6bece8efa679dd09fc2c333ef
darshil929 <darshilt929@gmail.com>
2024-03-25 20:43:36 +0530

    minor changes

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -6,9 +6,9 @@ export function CustomHeader({ navigation }: any) {
   const route = useRoute();
 
   return (
-    <Appbar.Header 
-    mode='center-aligned'
-    elevated={true}
+    <Appbar.Header
+      mode='center-aligned'
+      elevated={true}
     >
       <Appbar.Action icon="theme-light-dark" onPress={() => console.log('theme')} />
       <Appbar.Content title={route.name} />
