8007c9424339c551ce471bc6fa43f6951acc6f12
darshil929 <darshilt929@gmail.com>
2024-03-26 20:34:13 +0530

    added: near by services

---
+++ src/constants/constants.ts
@@ -0,0 +1,39 @@
+import hospital from '../assets/hospital.png'
+import police from '../assets/police.png'
+import fire from '../assets/fire.png'
+import paramedics from '../assets/paramedics.png'
+import rail from '../assets/rail.png'
+import blood from '../assets/blood.png'
+
+export const nearByService: NearByService[] = [
+    {
+        id: '1',
+        name: 'Hospital',
+        imgUrl: hospital,
+    },
+    {
+        id: '2',
+        name: 'Police Station',
+        imgUrl: police,
+    },
+    {
+        id: '3',
+        name: 'Fire Station',
+        imgUrl: fire,
+    },
+    {
+        id: '4',
+        name: 'ParaMedics',
+        imgUrl: paramedics,
+    },
+    {
+        id: '5',
+        name: 'Railway',
+        imgUrl: rail,
+    },
+    {
+        id: '6',
+        name: 'Blood Bank',
+        imgUrl: blood,
+    },
+];
--- src/screens/NearMe.tsx
+++ src/screens/NearMe.tsx
@@ -1,14 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, View, Image } from 'react-native'
 import React from 'react'
 
-import { CustomHeader } from '../components/CustomHeader'
+import { Text, Surface } from 'react-native-paper'
 
-const NearMe = ({props}: any) => {
+import { CustomHeader } from '../components/CustomHeader'
+import { nearByService } from '../constants/constants'
+
+const NearMe = ({ props }: any) => {
   return (
-    <CustomHeader {...props}/>
+    <>
+      <CustomHeader {...props} />
+      <View style={styles.container}>
+        <Text style={styles.header}>Near By Services</Text>
+        <View>
+          <FlatList
+            numColumns={2}
+            data={nearByService}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <Surface
+                style={styles.card}
+                elevation={3}
+              >
+                <Image source={item.imgUrl} style={styles.image} />
+                <Text>{item.name}</Text>
+              </Surface>
+            )}
+          />
+        </View>
+      </View>
+    </>
   )
 }
 
 export default NearMe
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 16,
+  },
+  card: {
+    height: 150,
+    width: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    margin: 16
+  },
+  image: {
+    height: 90,
+    width: 90,
+    marginBottom: 8
+  },
+})
